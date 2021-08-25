6efb5a43c36fad9c45b2055513b3c6a2cb8e77de
Grant <grantyli94@gmail.com>
2021-08-25 13:18:56 -0400

    added some listings routes

--- models/Listing.js
+++ models/Listing.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const db = require("../db");
-// const { BadRequestError, NotFoundError } = require("../expressError");
+const { BadRequestError, NotFoundError } = require("../expressError");
 // const { sqlForPartialUpdate } = require("../helpers/sql");
 
 class Listing {
@@ -20,6 +20,120 @@ class Listing {
     );
 
     const listing = result.rows[0];
+    return listing;
+  }
+
+  /** Create WHERE clause for filters, to be used by functions that query
+   * with filters.
+   *
+   * searchFilters (all optional):
+   * - minPrice
+   * - maxPrice
+   * - location (will find case-insensitive, partial matches)
+   *
+   * Returns {
+   *  where: "WHERE price <= $1 AND location ILIKE $2",
+   *  vals: [1000, '%new%']
+   * }
+   */
+
+  static _filterWhereBuilder({ minPrice, maxPrice, location }) {
+    let whereParts = [];
+    let vals = [];
+
+    if (minPrice !== undefined) {
+      vals.push(minPrice);
+      whereParts.push(`price >= $${vals.length}`);
+    }
+
+    if (maxPrice !== undefined) {
+      vals.push(maxPrice);
+      whereParts.push(`price <= $${vals.length}`);
+    }
+
+    if (location) {
+      vals.push(`%${location}%`);
+      whereParts.push(`location ILIKE $${vals.length}`);
+    }
+
+    const where = (whereParts.length > 0) ?
+      "WHERE " + whereParts.join(" AND ")
+      : "";
+
+    return { where, vals };
+  }
+
+  /** Find all companies (optional filter on searchFilters).
+   *
+   * searchFilters (all optional):
+   * - minPrice
+   * - maxPrice
+   * - location (will find case-insensitive, partial matches)
+   *
+   * Returns [{ id, title, description, location, price, username, image }, ...]
+   * */
+
+  static async findAll(searchFilters = {}) {
+    const { minPrice, maxPrice, location } = searchFilters;
+
+    if (minPrice > maxPrice) {
+      throw new BadRequestError("Min price cannot be greater than max price");
+    }
+
+    const { where, vals } = this._filterWhereBuilder({
+      minPrice, maxPrice, location,
+    });
+
+    const listingsRes = await db.query(`
+      SELECT id,
+             title,
+             description,
+             location,
+             price,
+             username,
+             image
+        FROM listings ${where}
+        ORDER BY title
+    `, vals);
+
+    return listingsRes.rows;
+  }
+
+  /** Given a listing id, return data about listing.
+ *
+ * Returns { id, title, description, location, price, username, image }
+ *
+ * Throws NotFoundError if not found.
+ **/
+
+  static async get(id) {
+    const listingRes = await db.query(
+      `SELECT id,
+                    title,
+                    description,
+                    location,
+                    price,
+                    username,
+                    image
+             FROM listings
+             WHERE id = $1`, [id]);
+
+    const listing = listingRes.rows[0];
+
+    if (!listing) throw new NotFoundError(`No listing: ${id}`);
+
+    // const companiesRes = await db.query(
+    //   `SELECT handle,
+    //                 name,
+    //                 description,
+    //                 num_employees AS "numEmployees",
+    //                 logo_url AS "logoUrl"
+    //          FROM companies
+    //          WHERE handle = $1`, [listing.companyHandle]);
+
+    // delete listing.companyHandle;
+    // listing.company = companiesRes.rows[0];
+
     return listing;
   }
 }
--- routes/listings.js
+++ routes/listings.js
@@ -21,4 +21,43 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
   return res.status(201).json({ newListing });
 });
 
+/** GET /  =>
+ *   { listings: [{ id, title, description, location, price, username, image }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - minPrice
+ * - maxPrice
+ * - location (will find case-insensitive, partial matches)
+ *
+ * Authorization required: logged in
+ */
+
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+  const q = req.query;
+  // arrive as strings from querystring, but we want as ints
+  if (q.minPrice !== undefined) q.minPrice = +q.minPrice;
+  if (q.maxPrice !== undefined) q.maxPrice = +q.maxPrice;
+
+  // const validator = jsonschema.validate(q, companySearchSchema);
+  // if (!validator.valid) {
+  //   const errs = validator.errors.map(e => e.stack);
+  //   throw new BadRequestError(errs);
+  // }
+
+  const listings = await Listing.findAll(q);
+  return res.json({ listings });
+});
+
+/** GET /[listingId] => { listing }
+ *
+ * Returns { id, title, description, location, price, username, image }
+ *
+ * Authorization required: logged in
+ */
+
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  const listing = await Listing.get(req.params.id);
+  return res.json({ listing });
+});
+
 module.exports = router;
